5196f8c0480ba1724123723fe11421dc13c42e1b
Jaya Chaturvedi <152374147+Jya2005@users.noreply.github.com>
2024-07-31 09:21:22 +0530

    Add files via upload

---
+++ inheritance.java
@@ -0,0 +1,9 @@
+package oops;
+
+public interface inheritance  {
+    public static void main(String[] args) {
+      //  Calc calc = new Calc();
+       // System.out.println(calc add());
+
+    }
+}
